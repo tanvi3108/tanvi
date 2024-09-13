eb78e6b2f3c98d38bc07027c31edce30be6622ef
tanvi3108 <haldauntanvi@gmail.com>
2024-09-13 23:38:58 +0530

    Message

--- lecture00/Helloworld.java
+++ lecture00/Helloworld.java
@@ -2,5 +2,11 @@ package lecture00;
 public class Helloworld{
     public static void main(String[] args) {
         System.out.println("hello world");
+        int a=23;
+        System.out.println(a);
+//ln -is used for next line 
+//system.in -is used for input
+//system.out -is used for output
+
     }
 }
