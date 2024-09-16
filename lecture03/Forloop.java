760707d1f1b3634bd941e30051e09371976072a0
tanvi3108 <haldauntanvi@gmail.com>
2024-09-16 20:42:22 +0530

    loops changes

--- lecture03/Forloop.java
+++ lecture03/Forloop.java
@@ -1,14 +1,69 @@
 package lecture03;
-
+import java.util.Scanner;
 public class Forloop {
     public static void main(String[] args) {
-        //WAP to print numbers from 0 to 10  
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n:\n"); 
+
+
+        //WAP to print numbers from 0 to 10 
+        // for(int i=0;i<=10;i++){
+        //     System.out.println(i);
+        // }
+
         //WAP to print first N even numbers
+        // int n = sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     if(i%2==0){
+        //         System.out.println(i);
+        //     }
+        // }
+
         //WAP to print first N odd numbers
+        // int n = sc.nextInt();
+        // for(int i=1; i<=n; i++) {
+        //     if(i%2!=0){
+        //         System.out.println(i);
+        //     }
+        // }
+
         //WAP to find the sum of first N natural numbers
+        // int n = sc.nextInt();
+        // int sum=0;
+        // for(int i=1; i<=n; i++){
+        //     sum +=i;
+        // }
+        // System.out.println(sum);
+
         //WAP to print first N natural numbers in reverse order
+        // int n=sc.nextInt();
+        // for(int i=n;i>=1; i--){
+        //     System.out.println(i);
+        // }
+
         //WAP to show two cases at once in for loop
+        // int n=10;
+        // for(int i=0, j=1; i<j&&j<n; i++,j++) {
+        //     System.out.println(i+" "+j);
+        // }
+        
         //WAP to know if a number is prime or not 
+        int n=sc.nextInt();
+        boolean b=true; //Number is prime or not
+
+        for(int i=2; i<n;i++){
+            if(n%i==0){
+                b=false;
+                break;
+            }
+                
+        }
+        if(b==true){
+            System.out.println("prime");
+        }else{
+            System.out.println("Not Prime");
+        }
 
 
     }
