613ff22df134ea72127b7b65ba6151a0a3f3d0a9
tanvi3108 <haldauntanvi@gmail.com>
2024-09-21 00:46:28 +0530

    loops

---
+++ lecture04/Try.java
@@ -0,0 +1,12 @@
+package lecture04;
+import java.util.Scanner;
+public class Try {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n= sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(n*i);
+        }
+    }
+}
